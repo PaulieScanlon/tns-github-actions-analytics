f1172f72a229e7377b80aebbaa43cf7cb486ff36
Paul Scanlon <pauliescanlon@gmail.com>
2024-06-19 15:43:05 +0100

    refactor: rename job

--- src/index.js
+++ src/index.js
@@ -13,17 +13,20 @@ const init = async () => {
       credentials,
     });
 
-    const test = await analyticsDataClient.runReport({
-      property: 'properties/282354357',
+    const [response] = await analyticsDataClient.runReport({
+      property: `properties/${process.env.GA4_PROPERTY_ID}`,
       dateRanges: [
         {
-          startDate: '2020-03-31',
+          startDate: '7daysAgo',
           endDate: 'today',
         },
       ],
       dimensions: [
         {
-          name: 'city',
+          name: 'fullPageUrl',
+        },
+        {
+          name: 'pageTitle',
         },
       ],
       metrics: [
@@ -31,89 +34,60 @@ const init = async () => {
           name: 'activeUsers',
         },
       ],
+      limit: 10,
+      metricAggregations: ['MAXIMUM'],
     });
 
-    console.log(test);
-
-    // const [response] = await analyticsDataClient.runReport({
-    //   property: `properties/${process.env.GA4_PROPERTY_ID}`,
-    //   dateRanges: [
-    //     {
-    //       startDate: '7daysAgo',
-    //       endDate: 'today',
-    //     },
-    //   ],
-    //   dimensions: [
-    //     {
-    //       name: 'fullPageUrl',
-    //     },
-    //     {
-    //       name: 'pageTitle',
-    //     },
-    //   ],
-    //   metrics: [
-    //     {
-    //       name: 'activeUsers',
-    //     },
-    //   ],
-    //   limit: 10,
-    //   metricAggregations: ['MAXIMUM'],
-    // });
-
-    // console.log(response);
-
     // Create the report string
-    // const report = response.rows
-    //   .map((row, index) => {
-    //     const { dimensionValues, metricValues } = row;
+    const report = response.rows
+      .map((row, index) => {
+        const { dimensionValues, metricValues } = row;
 
-    //     return `${index + 1}. <https://${dimensionValues[0].value}|${dimensionValues[1].value}> | *x${
-    //       metricValues[0].value
-    //     }*`;
-    //   })
-    //   .join('\\n')
-    //   .replace(/\\n/g, '\n');
-
-    // console.log(report);
+        return `${index + 1}. <https://${dimensionValues[0].value}|${dimensionValues[1].value}> | *x${
+          metricValues[0].value
+        }*`;
+      })
+      .join('\\n')
+      .replace(/\\n/g, '\n');
 
     // Post message to Slack
-    // fetch(process.env.SLACK_WEBHOOK_URL, {
-    //   method: 'POST',
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //   },
-    //   body: JSON.stringify({
-    //     blocks: [
-    //       {
-    //         type: 'header',
-    //         text: {
-    //           type: 'plain_text',
-    //           text: '📊 7 Days Google Analytics Report',
-    //           emoji: true,
-    //         },
-    //       },
-    //       {
-    //         type: 'divider',
-    //       },
-    //       {
-    //         type: 'section',
-    //         text: {
-    //           type: 'plain_text',
-    //           text: 'Top ten page views.',
-    //         },
-    //       },
-    //       {
-    //         type: 'context',
-    //         elements: [
-    //           {
-    //             type: 'mrkdwn',
-    //             text: report,
-    //           },
-    //         ],
-    //       },
-    //     ],
-    //   }),
-    // });
+    fetch(process.env.SLACK_WEBHOOK_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        blocks: [
+          {
+            type: 'header',
+            text: {
+              type: 'plain_text',
+              text: '📊 7 Days Google Analytics Report',
+              emoji: true,
+            },
+          },
+          {
+            type: 'divider',
+          },
+          {
+            type: 'section',
+            text: {
+              type: 'plain_text',
+              text: 'Top ten page views.',
+            },
+          },
+          {
+            type: 'context',
+            elements: [
+              {
+                type: 'mrkdwn',
+                text: report,
+              },
+            ],
+          },
+        ],
+      }),
+    });
   } catch (error) {
     console.error(error);
   }
