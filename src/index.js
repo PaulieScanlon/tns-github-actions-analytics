1cb195a6f3638e0d12c402630101ea988ee9c17e
Paul Scanlon <pauliescanlon@gmail.com>
2024-06-19 14:44:13 +0100

    test: test creds

--- src/index.js
+++ src/index.js
@@ -12,86 +12,88 @@ const init = async () => {
       Buffer.from(process.env.GOOGLE_APPLICATION_CREDENTIALS_BASE64, 'base64').toString('utf-8')
     );
 
-    const analyticsDataClient = new BetaAnalyticsDataClient({
-      credentials,
-    });
+    console.log('credentials: ', credentials);
 
-    const [response] = await analyticsDataClient.runReport({
-      property: `properties/${process.env.GA4_PROPERTY_ID}`,
-      dateRanges: [
-        {
-          startDate: '7daysAgo',
-          endDate: 'today',
-        },
-      ],
-      dimensions: [
-        {
-          name: 'fullPageUrl',
-        },
-        {
-          name: 'pageTitle',
-        },
-      ],
-      metrics: [
-        {
-          name: 'activeUsers',
-        },
-      ],
-      limit: 10,
-      metricAggregations: ['MAXIMUM'],
-    });
+    // const analyticsDataClient = new BetaAnalyticsDataClient({
+    //   credentials,
+    // });
 
-    const reportList = response.rows
-      .map((row, index) => {
-        const { dimensionValues, metricValues } = row;
+    // const [response] = await analyticsDataClient.runReport({
+    //   property: `properties/${process.env.GA4_PROPERTY_ID}`,
+    //   dateRanges: [
+    //     {
+    //       startDate: '7daysAgo',
+    //       endDate: 'today',
+    //     },
+    //   ],
+    //   dimensions: [
+    //     {
+    //       name: 'fullPageUrl',
+    //     },
+    //     {
+    //       name: 'pageTitle',
+    //     },
+    //   ],
+    //   metrics: [
+    //     {
+    //       name: 'activeUsers',
+    //     },
+    //   ],
+    //   limit: 10,
+    //   metricAggregations: ['MAXIMUM'],
+    // });
 
-        return `${index + 1}. <https://${dimensionValues[0].value}|${dimensionValues[1].value}> | *x${
-          metricValues[0].value
-        }*`;
-      })
-      .join('\\n')
-      .replace(/\\n/g, '\n');
+    // const reportList = response.rows
+    //   .map((row, index) => {
+    //     const { dimensionValues, metricValues } = row;
 
-    console.log(reportList);
+    //     return `${index + 1}. <https://${dimensionValues[0].value}|${dimensionValues[1].value}> | *x${
+    //       metricValues[0].value
+    //     }*`;
+    //   })
+    //   .join('\\n')
+    //   .replace(/\\n/g, '\n');
+
+    // console.log(reportList);
 
     //  --- post message to Slack
-    fetch(process.env.SLACK_WEBHOOK_URL, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        blocks: [
-          {
-            type: 'header',
-            text: {
-              type: 'plain_text',
-              text: '📊 7 Days Google Analytics Report',
-              emoji: true,
-            },
-          },
-          {
-            type: 'divider',
-          },
-          {
-            type: 'section',
-            text: {
-              type: 'plain_text',
-              text: 'Top ten page views.',
-            },
-          },
-          {
-            type: 'context',
-            elements: [
-              {
-                type: 'mrkdwn',
-                text: reportList,
-              },
-            ],
-          },
-        ],
-      }),
-    });
+    // fetch(process.env.SLACK_WEBHOOK_URL, {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: JSON.stringify({
+    //     blocks: [
+    //       {
+    //         type: 'header',
+    //         text: {
+    //           type: 'plain_text',
+    //           text: '📊 7 Days Google Analytics Report',
+    //           emoji: true,
+    //         },
+    //       },
+    //       {
+    //         type: 'divider',
+    //       },
+    //       {
+    //         type: 'section',
+    //         text: {
+    //           type: 'plain_text',
+    //           text: 'Top ten page views.',
+    //         },
+    //       },
+    //       {
+    //         type: 'context',
+    //         elements: [
+    //           {
+    //             type: 'mrkdwn',
+    //             text: reportList,
+    //           },
+    //         ],
+    //       },
+    //     ],
+    //   }),
+    // });
   } catch (error) {
     console.error('///// error');
     console.log(error);
